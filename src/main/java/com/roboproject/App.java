85d59898e5d558f55517734b6e16eb668770c464
Rafael Oliveira Lopes <rafaellopes.dev@gmail.com>
2025-04-23 16:13:55 -0300

    refactore(App): update the  with initial structure to receive commands from user.
    Added switchcase for the command list.

--- src/main/java/com/roboproject/App.java
+++ src/main/java/com/roboproject/App.java
@@ -1,32 +1,45 @@
+
 package com.roboproject;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Robo robo = new Robo();
-        for (int d = 0; d< 4; d++){
-            for (int j = 1; j<= 10; j++){
-                robo.printPosicao();
-                switch(d){
-                    case 0:
-                    robo.move(50 + j * 4, 50);
+public class App {
+    public static void main(String[] args) {
+        if (args.length != 0) {
+            String comando = args[0].toLowerCase();
+
+            switch (comando) {
+                case "--author":
+                    System.out.println("Autor: Rafael O. Lopes");
                     break;
-                    case 1:
-                    robo.move (90, 50 + j * 4);
+                case "--version":
+                    System.out.println("Versão: 1.0.0");
                     break;
-                    case 2:
-                    robo.move(90 - j * 4, 90);
+                case "--help":
+                    System.out.println("Use os seguintes comandos:");
+                    System.out.println("\t--author       Exibe o nome do autor");
+                    System.out.println("\t--version      Exibe a versão do programa");
+                    System.out.println("\t--help         Exibe esta mensagem");
+                    System.out.println("\t--comands      Lista os comandos de movimento");
+                    System.out.println("\t--move w a s d Move o robô conforme comandos");
                     break;
-                    case 3:
-                    robo.move(50, 90 - j * 4);
+                case "--comands":
+                    System.out.println("Lista de Comandos Aceitos:");
+                    System.out.println("\tw: move o robô para frente");
+                    System.out.println("\ta: move o robô para esquerda");
+                    System.out.println("\td: move o robô para direita");
+                    System.out.println("\ts: move o robô para trás");
+                    System.out.println("\t0: sai da aplicação");
                     break;
-                    default:
-                    System.out.println("Tecla inválida");
+                case "--move":
+                    Robo robo = new Robo();
+                    System.out.println("Movimentos agendados:");
+                    robo.movimentosAgendados(args);
                     break;
-                }
+                default:
+                    System.out.println("Comando inválido");
+                    break;
             }
+            System.exit(0);
         }
-        robo.printPosicao();
     }
 }
+
